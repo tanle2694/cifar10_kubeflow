a53707fa0ec5ea2c8e54081d5c782505f309a495
Tan Le Minh <tan.le2@ntq-solution.com.vn>
2021-06-17 16:11:50 +0700

    fix args update

--- pipeline_source/cifarlibs/utils/args_update.py
+++ pipeline_source/cifarlibs/utils/args_update.py
@@ -16,7 +16,7 @@ def args_update(original_parser, update_args):
     json_update_args = json.loads(update_args)
     json_original_args = vars(original_args)
     for key_arg, val_arg in json_update_args.items():
-        json_update_args[key_arg] = type(json_original_args[key_arg])(json_update_args[key_arg])
+        json_update_args[key_arg] = type(json_original_args[key_arg])(json_update_args[key_arg].replace("-", "_"))
 
     temp_args = argparse.Namespace()
     temp_args.__dict__.update(json_update_args)
